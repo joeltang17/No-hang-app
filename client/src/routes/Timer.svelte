5d4330cd4906d48c9345cfde2d73f24bd2c18809
Joel <joeltang17@gmail.com>
2023-10-09 15:36:11 -0400

    Finished the barebones

--- client/src/routes/Timer.svelte
+++ client/src/routes/Timer.svelte
@@ -1,65 +1,78 @@
 <script>
   let timerOn = false;
-  let Timer = 3; // Set the initial time in seconds
+  let time = [{sec: 3, type: 'crimp'}, {sec: 2, type: 'rest'}]
+  let currTimer = time[0].sec;
+  let currType = time[0].type; 
   let intervalId;
   let timeoutId; 
-  let time = [3, 2]
   const intervals = time.length;
   let i = 0;
-  let currTime; 
 
-  const start = (i, newTime) => {
+  const start = (newTime) => {
+    console.log(i)
     if(newTime){
-      Timer = time[i];
-    }
+      console.log('new time')
+      currTimer = time[i].sec;
+      currType = time[i].type;
+    } 
     if (!timerOn) {
+      timerOn = true;
       if(i === intervals - 1){
-        console.log('l')
-        timerOn = true;
+        console.log('last number')
+
         intervalId = setInterval(() => {
-        Timer--;
-        if (Timer < 1) {
-          clearInterval(intervalId);
-          timerOn = false;
+        currTimer--;
+        if (currTimer < 1) {
+          console.log('finish')
+          reset();
         }
         }, 1000);
+
       } 
       else {
-        console.log('a')
-        timerOn = true;
-        
+        console.log('not last number')
+
         intervalId = setInterval(() => {
-        Timer--;
-        if (Timer < 1) {
+        currTimer--;
+        if (currTimer < 1) {
           clearInterval(intervalId);
           timerOn = false;
         }
         }, 1000);
-        timeoutId = setTimeout(() => {start(i+1, true); console.log('p')}, Timer * 1000 + 1)
+        timeoutId = setTimeout(() => {i = i + 1; start(true);}, currTimer * 1000 + 1)
       }
 
     } 
     else {
-      clearTimeout(timeoutId)
+      console.log('paused')
+      clearTimeout(timeoutId);  
       clearInterval(intervalId);
       timerOn = false;
     }
   };
 
   const reset = () => {
+    console.log('reset')
+    i = 0;
     clearInterval(intervalId);
     clearTimeout(timeoutId);
     timerOn = false;
-    Timer = 3; // Reset the timer to the initial time
+    currTimer = time[0].sec;
+    currType = time[i].type; 
   };
 </script>
 
 <div class="d-flex align-items-center justify-content-center">
-  {Timer}
+  {currTimer}
 </div>
 
 <div class="d-flex align-items-center justify-content-center">
-    <button on:click={() => start(i, true)} type="button" class="btn btn-primary">{timerOn ? "Pause" : "Start"}</button>
+  {currType}
+</div>
+
+
+<div class="d-flex align-items-center justify-content-center">
+    <button on:click={() => start(false)} type="button" class="btn btn-primary">{timerOn ? "Pause" : "Start"}</button>
     <button on:click={reset} type="button" class="btn btn-secondary">Reset</button>
 </div>
 
