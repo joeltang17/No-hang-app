1b82300f6e15fa349255e3410316903bc1027486
Joel <joeltang17@gmail.com>
2023-10-09 00:17:00 -0400

    Made timer work with arrays

--- client/src/routes/Timer.svelte
+++ client/src/routes/Timer.svelte
@@ -1,21 +1,46 @@
 <script>
   let timerOn = false;
-  let Timer = 60; // Set the initial time in seconds
-  let intervalId; // Declare intervalId outside the function
+  let Timer = 3; // Set the initial time in seconds
+  let intervalId;
+  let timeoutId; 
+  let time = [3, 2]
+  const intervals = time.length;
+  let i = 0;
+  let currTime; 
 
-  const start = () => {
+  const start = (i, newTime) => {
+    if(newTime){
+      Timer = time[i];
+    }
     if (!timerOn) {
-      // Start the timer
-      timerOn = true;
-      intervalId = setInterval(() => {
+      if(i === intervals - 1){
+        console.log('l')
+        timerOn = true;
+        intervalId = setInterval(() => {
         Timer--;
-        if (Timer < 0) {
-          clearInterval(intervalId); // Stop the timer when it reaches 0
+        if (Timer < 1) {
+          clearInterval(intervalId);
           timerOn = false;
         }
-      }, 1000);
-    } else {
-      // Pause the timer
+        }, 1000);
+      } 
+      else {
+        console.log('a')
+        timerOn = true;
+        
+        intervalId = setInterval(() => {
+        Timer--;
+        if (Timer < 1) {
+          clearInterval(intervalId);
+          timerOn = false;
+        }
+        }, 1000);
+        timeoutId = setTimeout(() => {start(i+1, true); console.log('p')}, Timer * 1000 + 1)
+      }
+
+    } 
+    else {
+      clearTimeout(timeoutId)
       clearInterval(intervalId);
       timerOn = false;
     }
@@ -23,8 +48,9 @@
 
   const reset = () => {
     clearInterval(intervalId);
+    clearTimeout(timeoutId);
     timerOn = false;
-    Timer = 60; // Reset the timer to the initial time
+    Timer = 3; // Reset the timer to the initial time
   };
 </script>
 
@@ -33,7 +59,7 @@
 </div>
 
 <div class="d-flex align-items-center justify-content-center">
-    <button on:click={start} type="button" class="btn btn-primary">{timerOn ? "Pause" : "Start"}</button>
+    <button on:click={() => start(i, true)} type="button" class="btn btn-primary">{timerOn ? "Pause" : "Start"}</button>
     <button on:click={reset} type="button" class="btn btn-secondary">Reset</button>
 </div>
 
